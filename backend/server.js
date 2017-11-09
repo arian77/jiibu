b42dc4ce376d2747c5bce5615898929fc28b585a
Arian <arianmarkus77@yahoo.com>
2017-11-09 14:17:37 +0700

    Edit backend

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/", function(req, res) {
 // ........
 
 // Require Datas routes
-require("./app/routes/data.routes.js")(app);
+require("./app/routes/note.routes.js")(app);
 
 // ........
 
