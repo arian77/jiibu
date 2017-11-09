60e1ee38cde8b9361167d7ae6d2f7de9eedaa087
Arian <arianmarkus77@yahoo.com>
2017-11-09 15:55:35 +0700

    Create note.controller.js in folder (controllers)

---
+++ backend/app/controllers/note.controller.js
@@ -0,0 +1,21 @@
+var Note = require("../models/note.model.js");
+
+exports.create = function(req, res) {
+  // Create and Save a new Note
+};
+
+exports.findAll = function(req, res) {
+  // Retrieve and return all notes from the database.
+};
+
+exports.findOne = function(req, res) {
+  // Find a single note with a noteId
+};
+
+exports.update = function(req, res) {
+  // Update a note identified by the noteId in the request
+};
+
+exports.delete = function(req, res) {
+  // Delete a note with the specified noteId in the request
+};
